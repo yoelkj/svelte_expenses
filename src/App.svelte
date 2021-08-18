2e7de60e7859b85168eaeb7fe8fefd2cde3ed115
Yoel Velasquez Valencia <yoel.velasquez.valencia@gmail.com>
2021-08-18 00:06:17 -0500

    Add font-awesome and general styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,20 @@
-<script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
 	h1 {
-		color: #ff3e00;
+		color: purple;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
+<main>
+	<h1>Hello {name}!</h1>
+
+	<i class="fas fa-home fa-5x"></i>
+</main>
+
+
+<script>
+	export let name;
+</script>
